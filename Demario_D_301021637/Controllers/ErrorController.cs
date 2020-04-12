5b65fe8d864c1cd9fd0609d1d8860d54b703ad1a
Demario Douce <39357290+DemarioDouce@users.noreply.github.com>
2020-04-11 22:12:57 -0400

    Add files via upload

---
+++ Demario_D_301021637/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demario_D_301021637.Infrastructure;
+using Demario_D_301021637.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demario_D_301021637.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult Error() => View();
+    }
+}
